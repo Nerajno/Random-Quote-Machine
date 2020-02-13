39bc75e9d4f7d34d54475b87928e6c10c2823055
github username <github email address>
2020-02-13 13:07:51 -0500

    Post interview refactoring

--- js/rqm.js
+++ js/rqm.js
@@ -1,24 +1,19 @@
 $(document).ready(function(){
-
   getQuote();
 
   function getQuote(){
     var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-
    $.getJSON(url, function(data){
       $("#quote").html('"'+data.quoteText+' " ');
       $("#author").html(' - ' + data.quoteAuthor + ' ');
    });
-  
    $("#refreshBtn").one("click", function(){
      getQuote();
    });
- //attempts - 4
-   $(".twitter-share-button").click(function(){
-      window.open('https://twitter.com/intent/tweet?text='+$("#quote").text()+" "+$("#author").text());
-      // break; //break needed to kill multiple being opened when clicked
-    });
-
-
    };
+
+   $(".twitter-share-button").click(function(){
+    window.open('https://twitter.com/intent/tweet?text='+$("#quote").text()+" "+$("#author").text());
+    // break; //break needed to kill multiple being opened when clicked
+  });
 });
